474119c2c140e26c6970d91fa810777ff742a6af
Wojciech Dudek <wdudek82@gmail.com>
2019-01-13 23:06:35 +0100

    Added particle effect for background (simulating star paralax) #2

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField] private float backgroundScrollSpeed = 0.1f;
+    private Material _myMaterial;
+    private Vector2 _offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _myMaterial = GetComponent<Renderer>().material;
+        _offset = new Vector2(0, backgroundScrollSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _myMaterial.mainTextureOffset += _offset * Time.deltaTime;
+    }
+}
